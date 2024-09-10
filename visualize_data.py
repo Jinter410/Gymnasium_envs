549115fbaac45a83de1fff84df8957bd5ef28333
Jordan Cosio <jordan.cosio@outlook.fr>
2024-09-10 13:24:02 +0200

    Data is bounded

--- visualize_data.py
+++ visualize_data.py
@@ -20,9 +20,31 @@ def rotate_points(x, y, rotation_angle):
     y_rot = x * sin_angle + y * cos_angle
     return x_rot, y_rot
 
+def generate_one_turn(robot_x, robot_y, how, radius_min=2, radius_max=15, angle_min=70, angle_max=110, strength_min=0.5, strength_max=2) -> tuple:
+    # Turn parameters
+    radius = np.random.uniform(radius_min, radius_max)  # Rayon de courbure
+    angle = np.random.uniform(angle_min, angle_max)  # Angle de la courbe
+    strength = np.random.uniform(strength_min, strength_max)  # Force de la courbe
+
+    if how == 'right':
+        x, y = generate_right_turn(start_x = 0, start_y = 0, radius =  radius, angle = angle, strength = strength)
+    elif how == 'left':
+        x, y = generate_left_turn(start_x = 0, start_y = 0, radius = radius, angle = angle, strength = strength)
+
+    # Rotate turn to align it with the robot's direction
+    rotation_angle = inertia_angle - np.pi / 2
+    x_rot, y_rot = rotate_points(x, y, rotation_angle)
+
+    # Shift turn towards the robot
+    x_rot += robot_x
+    y_rot += robot_y
+    return x_rot, y_rot, radius, angle
+
 # N Robots visualisation
 num_robots = 10
-robot_positions = np.random.uniform(-10, 10, (num_robots, 2))
+robot_spawn = 10
+bounds = 20
+robot_positions = np.random.uniform(-robot_spawn, robot_spawn, (num_robots, 2))
 inertia_angles = np.random.uniform(0, 2 * np.pi, num_robots)
 inertia_length = 2
 scatter = False
@@ -34,20 +56,10 @@ for i in range(num_robots):
     robot_x, robot_y = robot_positions[i]
     inertia_angle = inertia_angles[i]
     
-    # Turn parameters
-    radius = np.random.uniform(5, 15)  # Rayon de courbure
-    angle = np.random.uniform(70, 110)  # Angle de la courbe
-    strength = np.random.uniform(0.5, 2)  # Force de la courbe
-
-    x, y = generate_left_turn(0, 0, radius, angle, strength)
-
-    # Rotate turn to align it with the robot's direction
-    rotation_angle = inertia_angle - np.pi / 2
-    x_rot, y_rot = rotate_points(x, y, rotation_angle)
-
-    # Shift turn towards the robot
-    x_rot += robot_x
-    y_rot += robot_y
+    x_rot, y_rot,radius, angle = generate_one_turn(robot_x, robot_y, 'right')
+    # If the turn is out of bounds
+    while np.any(np.abs(x_rot) > bounds * 0.9) or np.any(np.abs(y_rot) > bounds * 0.9):
+        x_rot, y_rot, radius, angle = generate_one_turn(robot_x, robot_y, 'right')
 
     if scatter:
         indices = np.linspace(0, len(x_rot) - 1, 5, dtype=int)
@@ -64,5 +76,7 @@ for i in range(num_robots):
 plt.xlabel('X position')
 plt.ylabel('Y position')
 plt.grid(True)
-plt.axis('equal')
+# Draw a red square around the bounds
+plt.plot([-bounds, bounds, bounds, -bounds, -bounds], [-bounds, -bounds, bounds, bounds, -bounds], 'r')
+plt.axis([-bounds - 5, bounds + 5, -bounds - 5, bounds + 5])
 plt.show()
