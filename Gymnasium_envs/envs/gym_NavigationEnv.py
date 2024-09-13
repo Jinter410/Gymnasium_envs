Fixed axis inversion errors : - Inverted axis on training to match pygame's - Adjusted plotting on env to have a correct scaling - Adjusted y-axis' inversion on every plot

Gymnasium_envs/envs/gym_NavigationEnv.py
@@ -213,9 +213,10 @@ class NavigationEnv(gym.Env):
         agent_radius = self.PHS * self.RATIO
         agent_speed, agent_angle = self.agent_vel
         arrow_pos = (
-            agent_center[0] + int(agent_speed * np.cos(agent_angle) * self.RATIO),  # Composante X
-            agent_center[1] - int(agent_speed * np.sin(agent_angle) * self.RATIO)   # Composante Y (inversée pour Pygame)
+            agent_center[0] + int(agent_speed * np.cos(agent_angle) * self.RATIO),  
+            agent_center[1] + int(agent_speed * np.sin(agent_angle) * self.RATIO)
         )
+        
         # Goal
         goal_color = (0, 0, 255)
         goal_pos_x = int((self.goal_pos[0] + self.W_BORDER) * self.RATIO)
@@ -243,8 +244,8 @@ class NavigationEnv(gym.Env):
             point_color = (177, 40, 229)
             for coord in self.coordinate_list:
                 point_x = int((coord[0] + self.W_BORDER) * self.RATIO)
-                # Inverser l'axe Y pour correspondre au système de coordonnées de Pygame
-                point_y = int((self.HEIGHT - coord[1] - self.H_BORDER) * self.RATIO)
+                point_y = int((coord[1] + self.H_BORDER) * self.RATIO)
+
                 pygame.draw.circle(self.window, point_color, (point_x, point_y), 5)
 
         

Gymnasium_envs/play.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import numpy as np
 import Gymnasium_envs
 import pygame
+from utils import manual_action
 
 def main(manual_control=False, n_rays=180, n_crowd=4, interceptor_percentage = 0.5, max_steps=100, render_mode="human"):
     pygame.init()
@@ -21,33 +22,7 @@ def main(manual_control=False, n_rays=180, n_crowd=4, interceptor_percentage = 0
                 done = True
 
         if manual_control:
-            keys = pygame.key.get_pressed()
-            
-            if not np.any(keys):
-                action = (0, 0)
-            else:
-                linear_velocity = 1
-                angle = None
-                
-                if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
-                    angle = -np.pi / 4 
-                elif keys[pygame.K_UP] and keys[pygame.K_LEFT]:
-                    angle = -3 * np.pi / 4  
-                elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
-                    angle = np.pi / 4  
-                elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
-                    angle = 3 * np.pi / 4  
-                elif keys[pygame.K_UP]:
-                    angle = -np.pi / 2 
-                elif keys[pygame.K_DOWN]:
-                    angle = np.pi / 2  
-                elif keys[pygame.K_LEFT]:
-                    angle = np.pi  
-                elif keys[pygame.K_RIGHT]:
-                    angle = 0  
-                    
-                if angle is not None:
-                    action = (linear_velocity, angle)
+            action = manual_action()
         else:
             action = env.action_space.sample()
 
@@ -58,4 +33,4 @@ def main(manual_control=False, n_rays=180, n_crowd=4, interceptor_percentage = 0
 
 if __name__ == "__main__":
     for i in range(100):
-        main(manual_control=False, n_rays= 40, n_crowd=10, interceptor_percentage=1, max_steps=700, render_mode="human")
+        main(manual_control=True, n_rays= 40, n_crowd=10, interceptor_percentage=1, max_steps=700, render_mode="human")

generate_data.py
@@ -8,6 +8,8 @@ import pygame
 from transformers import AutoTokenizer, AutoModel
 from tqdm import tqdm
 
+from utils import generate_one_turn, get_embeddings
+
 INSTRUCTIONS = {
     "left": [
         "Turn left.",
@@ -35,51 +37,6 @@ INSTRUCTIONS = {
     ]
 }
 
-def generate_right_turn(start_x, start_y, radius, angle, strength, steps=100):
-    t = np.linspace(0, np.radians(angle), steps)
-    x = start_x + radius * (1 - np.cos(t)) * strength
-    y = start_y + radius * np.sin(t)
-    return x, y
-
-def generate_left_turn(start_x, start_y, radius, angle, strength, steps=100):
-    t = np.linspace(0, np.radians(angle), steps)
-    x = start_x - radius * (1 - np.cos(t)) * strength
-    y = start_y + radius * np.sin(t)
-    return x, y
-
-def rotate_points(x, y, rotation_angle):
-    cos_angle = np.cos(rotation_angle)
-    sin_angle = np.sin(rotation_angle)
-    x_rot = x * cos_angle - y * sin_angle
-    y_rot = x * sin_angle + y * cos_angle
-    return x_rot, y_rot
-
-def generate_one_turn(robot_x, robot_y, how, inertia_angle, radius_min=2, radius_max=15, angle_min=70, angle_max=110, strength_min=0.5, strength_max=2) -> tuple:
-    # Turn parameters
-    radius = np.random.uniform(radius_min, radius_max)  # Rayon de courbure
-    angle = np.random.uniform(angle_min, angle_max)  # Angle de la courbe
-    strength = np.random.uniform(strength_min, strength_max)  # Force de la courbe
-
-    if how == 'right':
-        x, y = generate_right_turn(start_x = 0, start_y = 0, radius =  radius, angle = angle, strength = strength)
-    elif how == 'left':
-        x, y = generate_left_turn(start_x = 0, start_y = 0, radius = radius, angle = angle, strength = strength)
-
-    # Rotate turn to align it with the robot's direction
-    rotation_angle = inertia_angle - np.pi / 2
-    x_rot, y_rot = rotate_points(x, y, rotation_angle)
-
-    # # Shift turn towards the robot
-    # x_rot += robot_x
-    # y_rot += robot_y
-    return x_rot, y_rot, radius, angle
-
-def get_embeddings(model, tokenizer, sentences):
-    inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
-        embeddings = outputs.last_hidden_state.mean(dim=1)  # Moyenne des embeddings de tokens pour chaque phrase
-    return embeddings.cpu().numpy()
 
 def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, interceptor_percentage = 0.5, max_steps = 100, n_data =100, render_mode=None) -> Tuple[np.ndarray, np.ndarray]:
     pygame.init()
@@ -118,12 +75,13 @@ def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, inter
         while np.any(x_rot < -half_width + phs) or np.any(x_rot > half_width - phs) or \
             np.any(y_rot < -half_height + phs) or np.any(y_rot > half_height - phs):
             x_rot, y_rot, radius, angle = generate_one_turn(robot_x, robot_y, how, inertia_angle)
-                
+        
         # Scattering
         indices = np.linspace(0, len(x_rot) - 1, disc_output, dtype=int)
         x_rot = x_rot[indices]
         y_rot = y_rot[indices]
 
+        #############################
         # plt.plot(x_rot, y_rot, label=f'Robot {i+1}: rayon={radius:.2f}, angle={angle:.2f}°')
         # plt.plot(robot_x, robot_y, 'go', markersize=10)
         # plt.arrow(robot_x, robot_y, 2 * np.cos(inertia_angle), 2 * np.sin(inertia_angle),
@@ -134,9 +92,12 @@ def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, inter
         #     plt.text(x, y, str(idx + 1), fontsize=12, color='red', ha='center', va='center')
 
         # plt.axis([-env.WIDTH, env.WIDTH, -env.HEIGHT, env.HEIGHT])
+        # # # Invert Y axis to match pygame's coordinate system
+        # # plt.gca().invert_yaxis()
         # plt.show()
         # quit()
-
+        #############################
+        
         X[_] = np.concatenate([observation.flatten(), r_emb.flatten()])
         zipped_points = np.array([coord for pair in zip(x_rot, y_rot) for coord in pair])
         y[_] = zipped_points  # Utiliser les coordonnées zippées pour y
@@ -149,9 +110,9 @@ if __name__ == "__main__":
     model_name = "thenlper/gte-small"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-    X_left,y_left = generate("left", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
-    X_right,y_right = generate("right", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
+    X_left,y_left = generate("left", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=10000)
+    X_right,y_right = generate("right", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=10000)
     X = np.concat([X_left, X_right])
     y = np.concat([y_left, y_right])
-    np.save("./data/X_val.npy", X)
-    np.save("./data/y_val.npy", y)
+    np.save("./data/X.npy", X)
+    np.save("./data/y.npy", y)

tests/visualize_data.py
@@ -1,24 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def generate_right_turn(start_x, start_y, radius, angle, strength, steps=100):
-    t = np.linspace(0, np.radians(angle), steps)
-    x = start_x + radius * (1 - np.cos(t)) * strength
-    y = start_y + radius * np.sin(t)
-    return x, y
-
-def generate_left_turn(start_x, start_y, radius, angle, strength, steps=100):
-    t = np.linspace(0, np.radians(angle), steps)
-    x = start_x - radius * (1 - np.cos(t)) * strength
-    y = start_y + radius * np.sin(t)
-    return x, y
-
-def rotate_points(x, y, rotation_angle):
-    cos_angle = np.cos(rotation_angle)
-    sin_angle = np.sin(rotation_angle)
-    x_rot = x * cos_angle - y * sin_angle
-    y_rot = x * sin_angle + y * cos_angle
-    return x_rot, y_rot
+from utils import generate_left_turn, generate_right_turn, rotate_points
 
 def generate_one_turn(robot_x, robot_y, how, inertia_angle, radius_min=2, radius_max=15, angle_min=70, angle_max=110, strength_min=0.5, strength_max=2) -> tuple:
     # Turn parameters
@@ -56,10 +39,10 @@ for i in range(num_robots):
     robot_x, robot_y = robot_positions[i]
     inertia_angle = inertia_angles[i]
     
-    x_rot, y_rot,radius, angle = generate_one_turn(robot_x, robot_y, 'right', inertia_angle)
+    x_rot, y_rot,radius, angle = generate_one_turn(robot_x, robot_y, 'left', inertia_angle)
     # If the turn is out of bounds
     while np.any(np.abs(x_rot) > bounds * 0.9) or np.any(np.abs(y_rot) > bounds * 0.9):
-        x_rot, y_rot, radius, angle = generate_one_turn(robot_x, robot_y, 'right', inertia_angle)
+        x_rot, y_rot, radius, angle = generate_one_turn(robot_x, robot_y, 'left', inertia_angle)
 
     if scatter:
         indices = np.linspace(0, len(x_rot) - 1, 5, dtype=int)
@@ -79,4 +62,6 @@ plt.grid(True)
 # Draw a red square around the bounds
 plt.plot([-bounds, bounds, bounds, -bounds, -bounds], [-bounds, -bounds, bounds, bounds, -bounds], 'r')
 plt.axis([-bounds - 5, bounds + 5, -bounds - 5, bounds + 5])
+# Invert Y axis to match pygame's coordinate system
+plt.gca().invert_yaxis()
 plt.show()

viz_model.py
@@ -62,7 +62,6 @@ def main(checkpoint_path, env_name="Navigation-v0", n_rays=40, max_steps=100):
     while not done:
         action = env.action_space.sample()  # Action aléatoire (peut être remplacé par un modèle de contrôle)
         observation, reward, done, truncated, info = env.step(action)
-        
         env.render()  # Affiche l'environnement
         
         # Vérifier si l'utilisateur appuie sur "C"
@@ -70,12 +69,17 @@ def main(checkpoint_path, env_name="Navigation-v0", n_rays=40, max_steps=100):
             if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
                 print("En pause, entrez le nombre d'instructions :")
                 nb_instruction = input() 
-                observation[1] += np.pi  # Inverser l'angle d'inertie pour l'affichage
+                if not nb_instruction.isdigit():
+                    direct_instruction = nb_instruction
+                    nb_instruction = 1
                    
                 
                 for i in range(int(nb_instruction)):
-                    print(f"Instruction {i + 1} :")
-                    instruction = input()
+                    if not direct_instruction:
+                        print(f"Instruction {i + 1} :")
+                        instruction = input()
+                    else:
+                        instruction = direct_instruction
                     # Obtenir l'embedding de l'instruction
                     embedding = get_embeddings(text_model, tokenizer, [instruction])[0]
                     
@@ -83,20 +87,22 @@ def main(checkpoint_path, env_name="Navigation-v0", n_rays=40, max_steps=100):
                     output = generate_turn_points(observation, embedding, mlp_model)
                     x_points = output[::2]
                     y_points = output[1::2]
-
                     x_robot, y_robot = env.get_wrapper_attr('agent_pos')
-
                     
-                    ####4
-                    x_points_plot = x_points + x_robot
-                    y_points_plot = y_points + y_robot
+                    ############################
+                    x_points_plot = x_robot + x_points
+                    y_points_plot = y_robot + y_points
                     # Obtenir l'inertie (angle) du robot
-                    inertia_angle = observation[1] + np.pi# Angle d'inertie en radians
+                    inertia_angle = observation[1] 
 
                     # Plot simple avec matplotlib
                     plt.figure(figsize=(8, 8))
                     plt.plot(x_points_plot, y_points_plot, 'ro-', label="Trajectoire prédite")
                     plt.plot(x_robot, y_robot, 'go', markersize=10, label="Position du Robot")
+
+                    # Ajouter des numéros aux points
+                    for i, (x, y) in enumerate(zip(x_points_plot, y_points_plot)):
+                        plt.text(x, y, f'{i+1}', fontsize=12, ha='right')
 
                     # Dessiner une flèche pour l'inertie
                     arrow_length = 2
@@ -111,17 +117,14 @@ def main(checkpoint_path, env_name="Navigation-v0", n_rays=40, max_steps=100):
                     plt.legend()
                     plt.xlim([-20, 20])
                     plt.ylim([-20, 20])
+                    # Invert Y axis to match pygame's coordinate system
+                    plt.gca().invert_yaxis()
                     plt.show()
-
-                    ###
-
-                
-
+                    ############################
 
                 x_points += x_robot
-                y_points -= y_robot
+                y_points += y_robot
                 coordinates = list(zip(x_points, y_points))
-                
                 env.set_coordinate_list(coordinates)
                 env.render()
                 time.sleep(5)