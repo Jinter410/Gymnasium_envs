Fixed normalisation data not working properly

generate_data.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 import gymnasium as gym
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
@@ -86,14 +88,25 @@ def generate_sample(n_samples, env: gym.Env, robot_x: float, robot_y: float, ine
 
 def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, interceptor_percentage = 0.5, max_steps = 100, n_data =100, render_mode=None) -> Tuple[np.ndarray, np.ndarray]:
     pygame.init()
+
+    env_path = "./navigation/results/sac_Nav40Rays/models/rl_model_vecnormalize_999984_steps.pkl"
+
     env_name = "Navigation-v0"
     if env_name == "Navigation-v0":
-        env = gym.make(env_name, n_rays=n_rays, max_steps=max_steps, render_mode=render_mode)
+        kwargs = {'n_rays': n_rays, 'max_steps': max_steps, 'render_mode': render_mode}
     else:
-        env = gym.make(env_name, n_rays=n_rays, n_crowd=n_crowd, interceptor_percentage=interceptor_percentage, max_steps=max_steps, render_mode=render_mode)
+        kwargs = {'n_rays': n_rays, 'n_crowd': n_crowd, 'interceptor_percentage': interceptor_percentage, 'max_steps': max_steps, 'render_mode': render_mode}
+ 
 
+    env = VecNormalize.load(env_path, make_vec_env(env_name, n_envs=1, env_kwargs=kwargs))
     sentences = INSTRUCTIONS[how]
-    embeddings = get_embeddings(model, tokenizer, sentences)
+    
+    embeddings = np.zeros((len(sentences), 768))
+    for i,sentence in enumerate(sentences):
+        embedding_i = get_embeddings(model, tokenizer, [sentence])
+        embeddings[i] = embedding_i
+
+    print(sentences,embeddings)
 
     observation_size = env.observation_space.shape[0] + embeddings.shape[1]
     rot_size = disc_output * 2
@@ -104,16 +117,19 @@ def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, inter
     for _ in tqdm(range(n_data)):
         emb_i = np.random.choice(embeddings.shape[0], 1)
         r_emb = embeddings[emb_i]
+
         observation = env.reset()
         for i in range(n_steps):
-            action = env.action_space.sample()
-            observation, reward, done, truncated, info = env.step(action)
+            action = [env.action_space.sample()]
+            observation, reward, truncated, info = env.step(action)
             
-        inertia_angle = observation[1]
-        robot_x = env.get_wrapper_attr('agent_pos')[0]
-        robot_y = env.get_wrapper_attr('agent_pos')[1]
-        
-        samples_i = generate_sample(NUM_SAMPLES_PER_INSTRUCTION, env, robot_x, robot_y, inertia_angle, how, disc_output)
+        # Unnormalize only for the generate_sample function
+        unnormalized_observation = env.unnormalize_obs(observation)
+        inertia_angle = unnormalized_observation[0][1]
+        robot_x, robot_y = env.envs[0].get_wrapper_attr('agent_pos')
+
+        # Generate real-world coordinates with unnormalized values
+        samples_i = generate_sample(NUM_SAMPLES_PER_INSTRUCTION, env.envs[0], robot_x, robot_y, inertia_angle, how, disc_output)
 
         #############################
         # plt.plot(x_rot, y_rot, label=f'Robot {i+1}: rayon={radius:.2f}, angle={angle:.2f}°')
@@ -132,7 +148,7 @@ def generate(how, model, tokenizer, disc_output = 5, n_rays=40, n_crowd=4, inter
         # quit()
         #############################
         
-        X[_] = np.concatenate([observation.flatten(), r_emb.flatten()])
+        X[_] = np.concatenate([observation[0].flatten(), r_emb.flatten()])
         # zipped_points = np.array([coord for pair in zip(x_rot, y_rot) for coord in pair])
         y[_] = samples_i.reshape(NUM_SAMPLES_PER_INSTRUCTION, -1)
         n_steps = np.random.randint(2, 5)
@@ -144,11 +160,11 @@ if __name__ == "__main__":
     model_name = "roberta-base"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-    X_left,y_left = generate("left", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
-    X_right,y_right = generate("right", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
-    X_forward,y_forward = generate("forward", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
-    X_backwards,y_backwards = generate("backwards", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=100)
+    X_right,y_right = generate("right", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=2000)
+    X_left,y_left = generate("left", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=2000)
+    X_forward,y_forward = generate("forward", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=2000)
+    X_backwards,y_backwards = generate("backwards", model, tokenizer, disc_output = 5, n_rays= 40, max_steps=10, n_data=2000)
     X = np.concat([X_left, X_right, X_forward, X_backwards])
     y = np.concat([y_left, y_right, y_forward, y_backwards])
-    np.save("./data/X_test.npy", X)
-    np.save("./data/y_test.npy", y)
+    np.save("./data/X_normalized.npy", X)
+    np.save("./data/y_normalized.npy", y)

train.py
@@ -11,13 +11,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 run_name = "256_128_neur+forward+backwards+Roberta+MinMSELoss"
 train_percentage = 0.7
-n_epochs = 300
+n_epochs = 200
 save_freq = 10
 fc_size1 = 256
 fc_size2 = 128
 
-X = np.load('./data/X.npy')
-y = np.load('./data/y.npy')
+X = np.load('./data/X_normalized.npy')
+y = np.load('./data/y_normalized.npy')
 
 indices = np.random.permutation(X.shape[0])
 X = X[indices]

utils.py
@@ -101,9 +101,10 @@ def generate_one(robot_x, robot_y, how, inertia_angle, radius_min=2, radius_max=
 
     return x_rot, y_rot, radius, angle
 
-def get_embeddings(model, tokenizer, sentences):
+def get_embeddings(model, tokenizer, sentences) -> np.ndarray:
+    model.eval()  # Set the model to evaluation mode
     inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
     with torch.no_grad():
         outputs = model(**inputs)
-        embeddings = outputs.last_hidden_state.mean(dim=1)  # Moyenne des embeddings de tokens pour chaque phrase
+        embeddings = outputs.last_hidden_state.mean(dim=1)  # Mean pooling over the sequence length
     return embeddings.cpu().numpy()

viz_errors.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from utils import load_model
 
 # Charger les données
-X_test = np.load('./data/X_test.npy')  # Données d'observations
-y_test = np.load('./data/y_test.npy')  # Données de vérité terrain (trajectoires réelles)
+X_test = np.load('./data/X_test_normalized.npy')  # Données d'observations
+y_test = np.load('./data/y_test_normalized.npy')  # Données de vérité terrain (trajectoires réelles)
 
 # Paramètres
-n_robots = 10  # Nombre de robots à afficher (modifier selon tes besoins)
-checkpoint_path = './models/256_128_neur+forward+backwards+Roberta+MinMSELoss/model_epoch_100.pth'  # Chemin du checkpoint
+n_robots = 20  # Nombre de robots à afficher (modifier selon tes besoins)
+checkpoint_path = './models/256_128_neur+forward+backwards+Roberta+MinMSELoss/model_epoch_200.pth'  # Chemin du checkpoint
 
 # Paramètres du modèle
 input_size = X_test.shape[1]

viz_model.py
@@ -13,19 +13,13 @@ import pygame
 from typing import Tuple
 from tqdm import tqdm
 
-from utils import load_model
+from utils import load_model, get_embeddings
 
 def c2p(cart):
         r = np.linalg.norm(cart)
         theta = np.arctan2(cart[1], cart[0])
         return np.array([r, theta])
 
-def get_embeddings(model, tokenizer, sentences):
-    inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
-        embeddings = outputs.last_hidden_state.mean(dim=1)
-    return embeddings.cpu().numpy()
 
 def generate_turn_points(observation, embedding, model):
     # Concaténer les observations et l'embedding
@@ -61,7 +55,6 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
     done = False
     # Load model
     kwargs = {'n_rays': 40, 'max_steps': 150}
-
     kwargs['render_mode'] = 'human'
     model_path = "./navigation/results/sac_Nav40Rays/models/rl_model_999984_steps.zip"
     env_path = "./navigation/results/sac_Nav40Rays/models/rl_model_vecnormalize_999984_steps.pkl"
@@ -74,11 +67,9 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
         action, _states = model.predict(observation, deterministic=True)
         # After receiving the observation from env.step()
         observation, reward, done, info = env.step(action)
+
         if done:
             env.envs[0].unwrapped.set_coordinate_list([])
-
-        # Unnormalize the observation
-        unnormalized_obs = env.unnormalize_obs(observation)
         
         if len(objectives) > 0:
             curr_objective = objectives[0].copy()
@@ -89,6 +80,9 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
             curr_objective -= env.envs[0].get_wrapper_attr('agent_pos')
 
             curr_objective_polar = c2p(curr_objective)
+
+            # Unnormalize the observation
+            unnormalized_obs = env.unnormalize_obs(observation)
 
             # Inject the unnormalized waypoint into the unnormalized observation
             unnormalized_obs[0][2:4] = curr_objective_polar
@@ -104,18 +98,16 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
             if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
                 instruction = np.random.choice([
                     "Turn left.",
-                    "Turn right.", 
+                    "Turn right.",
                     "Move forward.",
                     "Move backwards."
                     ])
                 print(f"Instruction: {instruction}")
                 # Obtenir l'embedding de l'instruction
                 embedding = get_embeddings(text_model, tokenizer, [instruction])[0]
-
-                unnormalized_obs = env.unnormalize_obs(observation)
                 
                 # Generate the output using the unnormalized observation
-                output = generate_turn_points(unnormalized_obs[0], embedding, mlp_model)
+                output = generate_turn_points(observation[0].flatten(), embedding.flatten(), mlp_model)
                 x_points = output[::2]
                 y_points = output[1::2]
                 x_robot, y_robot = env.envs[0].get_wrapper_attr('agent_pos')
@@ -151,7 +143,7 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
 
                 # # Dessiner une flèche pour l'inertie
                 # arrow_length = 2
-            n_steps = np.random.randint(2, 5)
+                
                 # plt.arrow(x_robot, y_robot, arrow_length * np.cos(inertia_angle), arrow_length * np.sin(inertia_angle),
                 #         head_width=0.5, head_length=0.5, fc='blue', ec='blue', label="Inertie")
 
@@ -175,6 +167,6 @@ def main(checkpoint_path, nlp_model, env_name="Navigation-v0", n_rays=40, max_st
 
 # Exemple d'appel à la fonction principale
 if __name__ == '__main__':
-    checkpoint_path = './models/256_128_neur+forward+backwards+Roberta+MinMSELoss/model_epoch_200.pth'  # Remplacer par le chemin de votre checkpoint
+    checkpoint_path = './models/256_128_neur+forward+backwards+Roberta+MinMSELoss/model_epoch_200.pth'
     model_name = "roberta-base"
     main(checkpoint_path, model_name)